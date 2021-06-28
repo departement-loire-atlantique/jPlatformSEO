2ac3ad040a78498c55a86f066607b2ae71b5a418
LChoquet <lucas.choquet@trsb.net>
2021-06-28 09:44:45 +0200

    Fix message d'erreur taille des champs

--- WEB-INF/classes/fr/cg44/plugin/seo/datacontroller/TitreSeoPublicationController.java
+++ WEB-INF/classes/fr/cg44/plugin/seo/datacontroller/TitreSeoPublicationController.java
@@ -11,9 +11,12 @@ public class TitreSeoPublicationController extends BasicDataController implement
 
    public ControllerStatus checkIntegrity(Data data) {
      Publication pub = (Publication)data ;
-     if (Util.notEmpty(pub.getExtraData("extra.Content.jcmsplugin.seo.titreseo")) && pub.getExtraData("extra.Content.jcmsplugin.seo.titreseo").length() > 60) {
-       return new ControllerStatus("Le résumé doit contenir au moins 300 caractères");
+     if (Util.notEmpty(pub.getExtraData("extra.Content.jcmsplugin.seo.titreseo.fr")) && pub.getExtraData("extra.Content.jcmsplugin.seo.titreseo.fr").length() > 60) {
+         return new ControllerStatus("Le champ Titre Seo FR doit contenir moins de 60 caractères.");
      }
-      return ControllerStatus.OK ;
+     if (Util.notEmpty(pub.getExtraData("extra.Content.jcmsplugin.seo.titreseo.en")) && pub.getExtraData("extra.Content.jcmsplugin.seo.titreseo.en").length() > 60) {
+         return new ControllerStatus("Le champ Titre Seo EN doit contenir moins de 60 caractères.");
+     }
+     return ControllerStatus.OK ;
    }
 } 
